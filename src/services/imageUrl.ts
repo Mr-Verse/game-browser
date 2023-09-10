0d8581cd7f3740d5341386756f6cabc4bed522fe
Mr-Verse <hdmail010@gmail.com>
2023-09-10 11:43:37 +0530

    Handling games without images.

--- src/services/imageUrl.ts
+++ src/services/imageUrl.ts
@@ -1,5 +1,6 @@
 export function getCroppedImageUrl(url: string | null) {
-  if (!url) return;
+  if (!url)
+    return "https://upload.wikimedia.org/wikipedia/commons/d/d1/Image_not_available.png";
   const index = url.indexOf("media/") + "media/".length;
   const urlCrop = url.slice(0, index) + "crop/600/400/" + url.slice(index);
   return urlCrop;
